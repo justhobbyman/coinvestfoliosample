e2716bf7faad03a963c4039e4e75f4679a8ded87
justhobbyman <144333754+justhobbyman@users.noreply.github.com>
2023-09-12 16:05:42 +0900

    Update gnmenu.js

--- js/gnmenu.js
+++ js/gnmenu.js
@@ -39,54 +39,37 @@
 			};
 		},
 		_initEvents : function() {
-			var self = this;
-
-			if( !mobilecheck() ) {
-				this.trigger.addEventListener( 'mouseover', function(ev) { self._openIconMenu(); } );
-				this.trigger.addEventListener( 'mouseout', function(ev) { self._closeIconMenu(); } );
-			
-				this.menu.addEventListener( 'mouseover', function(ev) {
-					self._openMenu(); 
-					document.addEventListener( self.eventtype, self.bodyClickFn ); 
-				} );
-			}
-			this.trigger.addEventListener( this.eventtype, function( ev ) {
-				ev.stopPropagation();
-				ev.preventDefault();
-				if( self.isMenuOpen ) {
-					self._closeMenu();
-					document.removeEventListener( self.eventtype, self.bodyClickFn );
-				}
-				else {
-					self._openMenu();
-					document.addEventListener( self.eventtype, self.bodyClickFn );
-				}
-			} );
-			this.menu.addEventListener( this.eventtype, function(ev) { ev.stopPropagation(); } );
-		},
-		_openIconMenu : function() {
-			classie.add( this.menu, 'gn-open-part' );
-		},
-		_closeIconMenu : function() {
-			classie.remove( this.menu, 'gn-open-part' );
-		},
-		_openMenu : function() {
-			if( this.isMenuOpen ) return;
-			classie.add( this.trigger, 'gn-selected' );
-			this.isMenuOpen = true;
-			classie.add( this.menu, 'gn-open-all' );
-			this._closeIconMenu();
-		},
-		_closeMenu : function() {
-			if( !this.isMenuOpen ) return;
-			classie.remove( this.trigger, 'gn-selected' );
-			this.isMenuOpen = false;
-			classie.remove( this.menu, 'gn-open-all' );
-			this._closeIconMenu();
+		        var self = this;
+		
+		        this.trigger.addEventListener( this.eventtype, function( ev ) {
+		            ev.stopPropagation();
+		            ev.preventDefault();
+		            if( self.isMenuOpen ) {
+		                self._closeMenu();
+		                document.removeEventListener( self.eventtype, self.bodyClickFn );
+		            }
+		            else {
+		                self._openMenu();
+		                document.addEventListener( self.eventtype, self.bodyClickFn );
+		            }
+		        } );
+		        this.menu.addEventListener( this.eventtype, function(ev) { ev.stopPropagation(); } );
+		    },
+		    _openMenu : function() {
+		        if( this.isMenuOpen ) return;
+		        classie.add( this.trigger, 'gn-selected' );
+		        this.isMenuOpen = true;
+		        classie.add( this.menu, 'gn-open-all' );
+		    },
+		    _closeMenu : function() {
+		        if( !this.isMenuOpen ) return;
+		        classie.remove( this.trigger, 'gn-selected' );
+		        this.isMenuOpen = false;
+		        classie.remove( this.menu, 'gn-open-all' );
+		    }
 		}
-	}
 
-	// add to global namespace
-	window.gnMenu = gnMenu;
+// add to global namespace
+window.gnMenu = gnMenu;
 
 } )( window );
