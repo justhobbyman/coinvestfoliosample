70577c2e217da991a38a80302d34b6880b7772fa
justhobbyman <144333754+justhobbyman@users.noreply.github.com>
2023-09-12 19:07:09 +0900

    Update spot-calculator.js

--- js/spot-calculator.js
+++ js/spot-calculator.js
@@ -39,3 +39,60 @@ function calculateProfitLoss() {
             }
         }
 
+ function updateInputValue(inputId, value) {
+            const inputElement = document.getElementById(inputId);
+            if (value === "custom") {
+                inputElement.style.display = "block";
+            } else {
+                inputElement.style.display = "none";
+                inputElement.value = value;
+            }
+        }
+
+function sharePNL() {
+            const spotResultPNL = document.getElementById('resultPNL').innerText;
+            const spotResultROE = document.getElementById('resultROE').innerText;
+            
+            let shareText = spotResultPNL + '\n' + spotResultROE;
+            
+            // 현재 페이지의 URL 추가
+            shareText += '\n\n' + window.location.href;
+        
+            copyToClipboard(shareText);
+            alert('Your result and website link have been copied to the clipboard. Paste it wherever you like!');
+        }
+
+function copyToClipboard(text) {
+            const textArea = document.createElement("textarea");
+            textArea.value = text;
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+        
+            try {
+                document.execCommand('copy');
+            } catch (err) {
+                console.error('Failed to copy text: ', err);
+            }
+        
+            document.body.removeChild(textArea);
+        }
+
+window.onload = function() {
+            const urlParams = new URLSearchParams(window.location.search);
+            const sharedResult = urlParams.get('result');
+            if (sharedResult) {
+                document.getElementById('result').innerText = sharedResult;
+
+                const profitLossValue = parseFloat(sharedResult.split('$')[1]);
+                const resultElement = document.getElementById('result');
+        
+                if (profitLossValue < 0) {
+                    resultElement.style.color = 'blue';
+                } else if (profitLossValue > 0) {
+                    resultElement.style.color = 'red';
+                } else {
+                    resultElement.style.color = '#FFFFFF';
+                }
+            }  
+        }
